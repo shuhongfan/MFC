1caa4de7cc45437d96b4b2164667ec4181eacbe6
SHF <shuhongfan@live.com>
2021-11-12 18:15:00 +0800

    2021.11.8 MFC 湖工 实验一

---
+++ demo1/调查问卷/调查问卷/resource.h
@@ -0,0 +1,31 @@
+﻿//{{NO_DEPENDENCIES}}
+// Microsoft Visual C++ 生成的包含文件。
+// 供 My.rc 使用
+//
+#define IDM_ABOUTBOX                    0x0010
+#define IDD_ABOUTBOX                    100
+#define IDS_ABOUTBOX                    101
+#define IDD_MY_DIALOG                   102
+#define IDR_MAINFRAME                   128
+#define age1                            1000
+#define age2                            1002
+#define age3                            1003
+#define type1                           1004
+#define type2                           1006
+#define type3                           1007
+#define goal1                           1010
+#define goal2                           1011
+#define goal3                           1012
+#define IDC_CHECK4                      1013
+#define goal4                           1013
+
+// Next default values for new objects
+// 
+#ifdef APSTUDIO_INVOKED
+#ifndef APSTUDIO_READONLY_SYMBOLS
+#define _APS_NEXT_RESOURCE_VALUE        130
+#define _APS_NEXT_COMMAND_VALUE         32771
+#define _APS_NEXT_CONTROL_VALUE         1014
+#define _APS_NEXT_SYMED_VALUE           101
+#endif
+#endif
